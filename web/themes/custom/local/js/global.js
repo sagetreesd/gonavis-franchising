b1872f670d69196d305172dff35aab2a76a13cb8
John Bocalig <john@sagetree.net>
2018-06-01 12:22:37 -0700

    ABFS-9 More footer theming, social media links.

--- web/themes/custom/local/js/global.js
+++ web/themes/custom/local/js/global.js
@@ -9,7 +9,13 @@
 
   Drupal.behaviors.bootstrap_barrio_subtheme = {
     attach: function (context, settings) {
-
+      $('body').on('mouseenter mouseleave', '.dropdown', function (e) {
+        var _d = $(e.target).closest('.dropdown'); _d.addClass('show');
+        setTimeout(function () {
+          _d[_d.is(':hover') ? 'addClass' : 'removeClass']('show');
+          $('[data-hover="dropdown"]', _d).attr('aria-expanded', _d.is(':hover'));
+        }, 300);
+      });
     }
   };
 
